855c4bc8b1e497261d431a751045f671a0405856
Toan0409 <Tranquoctoan04092004@gmail.com>
2025-11-02 23:44:03 +0700

    page title Destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Footer from "./components/footer/Footer";
+import Home from "./components/Home"
+import React from "react";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
+      <Home />
       <Footer />
-    </div>
+    </>
   );
 }
 
