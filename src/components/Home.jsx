cf49920b074836e71b04e87ab80846c5bc48b6cd
Toan0409 <Tranquoctoan04092004@gmail.com>
2025-11-03 16:39:20 +0700

    productlistings component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
+import products from "../data/product";
 import PageHeading from "./PageHeading";
-
+import ProductListings from "./ProductListings";
 export default function Home() {
     return (
         <div className="home-container">
 
             <PageHeading title="Explore Eazy Stickers!">
-                <p className='page-heading-paragraph'>
-                    Add a touch of creativity to your world with our vibrant stickers. Perfect for any occasion!
-                </p>
+                Add a touch of creativity to your world with our vibrant stickers. Perfect for any occasion!
             </PageHeading>
+            <ProductListings products={products} />
         </div>
     );
 }
