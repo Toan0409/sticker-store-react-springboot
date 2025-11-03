baa7982dfa6ecafaeef054b045b22399de749c15
Toan0409 <Tranquoctoan04092004@gmail.com>
2025-11-03 15:23:05 +0700

    page heading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,12 @@ import PageHeading from "./PageHeading";
 export default function Home() {
     return (
         <div className="home-container">
-            <PageHeading />
+
+            <PageHeading title="Explore Eazy Stickers!">
+                <p className='page-heading-paragraph'>
+                    Add a touch of creativity to your world with our vibrant stickers. Perfect for any occasion!
+                </p>
+            </PageHeading>
         </div>
     );
 }
