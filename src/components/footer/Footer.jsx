594dbc814f7e749266578b347feed70feee81de5
Toan0409 <Tranquoctoan04092004@gmail.com>
2025-11-10 15:48:07 +0700

    styling Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import "./footer.css";
 
 export default function Footer() {
     return (
-        <footer className="footer">
+        <footer className="flex justify-center py-4 font-primary text-gray-700">
             Built with
-            <FontAwesomeIcon icon={faHeart} className="fa-icon" aria-hidden="true" />
-            by <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">Trần Quốc Toàn</a>
+            <FontAwesomeIcon
+                icon={faHeart}
+                className="text-red-600 mx-1 animate-pulse"
+                aria-hidden="true" />
+            by
+            <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="text-primary font-semibold px-1 transition-colors duration-300 hover:text-dark"
+            >
+                Trần Quốc Toàn
+            </a>
         </footer>
     );
 }
