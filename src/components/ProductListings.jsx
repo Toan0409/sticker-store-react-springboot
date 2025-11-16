d219ab1641090d4161ff6a8785f744c2a4f11636
Toan0409 <Tranquoctoan04092004@gmail.com>
2025-11-16 11:50:02 +0700

    fix search and sort component

--- src/components/ProductListings.jsx
+++ src/components/ProductListings.jsx
@@ -58,8 +58,8 @@ export default function ProductListings({ products }) {
                 />
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
-                {products.length > 0 ? (
-                    products.map((product) => <ProductCard key={product.productId} product={product} />)
+                {filteredAndSortedProducts.length > 0 ? (
+                    filteredAndSortedProducts.map((product) => <ProductCard key={product.productId} product={product} />)
                 ) : (
                     <p className="text-center font-primary font-bold text-lg text-primary">No products available.</p>
                 )}
