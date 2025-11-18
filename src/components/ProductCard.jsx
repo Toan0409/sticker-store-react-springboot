4e38665373dca9fe3b5517a12ecce1087438e46c
Toan0409 <Tranquoctoan04092004@gmail.com>
2025-11-18 15:29:51 +0700

    complete dark mode

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -13,8 +13,8 @@ export default function ProductCard({ product }) {
                 />
             </div>
             <div className="relative h-48 p-4 flex flex-col font-primary">
-                <h2 className="text-xl font-semibold text-primary mb-2">{product.name}</h2>
-                <p className="text-base text-gray-600 mb-4">{product.description}</p>
+                <h2 className="text-xl font-semibold text-primary mb-2 dark:text-light">{product.name}</h2>
+                <p className="text-base text-gray-600 mb-4 dark:text-light">{product.description}</p>
                 <div className="flex items-center justify-between mt-auto">
                     <div className="bg-lighter text-primary font-medium text-sm py-2 px-4 rounded-tl-md">
                         <Price currency="$" price={product.price} />
